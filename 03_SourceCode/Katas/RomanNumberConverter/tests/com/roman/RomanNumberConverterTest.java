dd02fee108687e9f1d29f4c6f8b63ee2d1750e22
HerolindHajredini <71695706+HerolindHajredini@users.noreply.github.com>
2021-11-12 12:46:09 +0100

    Updated Project Folder and new Source Code

---
+++ 03_SourceCode/Katas/RomanNumberConverter/tests/com/roman/RomanNumberConverterTest.java
@@ -0,0 +1,62 @@
+package com.roman;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class RomanNumberConverterTest {
+    @Test
+    public void itShouldReturnI_Given1(){
+        assertEquals("I", RomanNumberConverter.toRoman(1));
+    }
+
+    @Test
+    public void itShouldReturnII_Given2() {
+        assertEquals("II", RomanNumberConverter.toRoman(2));
+
+    }
+
+    @Test
+    public void itShouldReturnIII_Given3() {
+        assertEquals("III", RomanNumberConverter.toRoman(3));
+
+    }
+
+    @Test
+    public void itShouldReturnV_Given5() {
+        assertEquals("V", RomanNumberConverter.toRoman(5));
+
+    }
+
+    @Test
+    void itShouldReturnVI_Given6() {
+        assertEquals("VI", RomanNumberConverter.toRoman(6));
+        assertEquals("VII", RomanNumberConverter.toRoman(7));
+    }
+
+    @Test
+    void itShouldReturnX_Given10() {
+        assertEquals("X", RomanNumberConverter.toRoman(10));
+    }
+
+    @Test
+    public void itShouldReturnXX_Given20() {
+        assertEquals("XX", RomanNumberConverter.toRoman(20));
+    }
+
+    @Test
+    public void itShouldReturnIV_Given4() {
+        assertEquals("IV", RomanNumberConverter.toRoman(4));
+    }
+
+    @Test
+    public void itShouldReturnIX_Given9() {
+        assertEquals("IX", RomanNumberConverter.toRoman(9));
+    }
+
+    @Test
+    void itShouldReturnXXXVII_Given37() {
+        assertEquals("XXXVII", RomanNumberConverter.toRoman(37));
+
+    }
+}
