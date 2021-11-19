7077b3be7b8f404712ba8591e01c320d1de9501f
Amgad <amgadhammash1@gmail.com>
2021-11-19 18:33:50 +0100

    couple tests later

--- 03_SourceCode/Katas/RomanNumberConverter/src/at/htlleonding/RomanNumberConverter/RomanNumberConverter.java
+++ 03_SourceCode/Katas/RomanNumberConverter/src/at/htlleonding/RomanNumberConverter/RomanNumberConverter.java
@@ -8,5 +8,58 @@
  * Description:
  * ----------------------------------------------------------
  */
-package at.htlleonding.RomanNumberConverter;public class RomanNumberConverter {
+package at.htlleonding.RomanNumberConverter;
+
+public class RomanNumberConverter {
+
+    public static String toRoman(int arabicNumber) {
+        var romanNumber = "";
+        for (var numeral : Numeral.values()){
+            while (arabicNumber >= numeral.mArabic) {
+                romanNumber += numeral.mRoman;
+                arabicNumber -= numeral.mArabic;
+            }
+        }
+        return romanNumber;
+    }
+
+    public static int toArabic(String roman) {
+        int arabic = 0;
+
+        if(roman == "V") {
+            arabic += 5;
+        }
+        if(roman == "IV") {
+            arabic += 4;
+        }
+        if(roman == "III") {
+            arabic += 3;
+        }
+        if(roman == "II") {
+            arabic += 2;
+        }
+        if(roman == "I") {
+            arabic += 1;
+        }
+
+        return arabic;
+    }
+
+
+    enum Numeral{
+        TEN(10,"X"),
+        NINE(9, "IX"),
+        FIVE(5, "V"),
+        FOUR(4, "IV"),
+        ONE(1, "I");
+
+        private final int mArabic;
+        private final String mRoman;
+
+        Numeral(int arabic, String roman) {
+
+            mArabic = arabic;
+            mRoman = roman;
+        }
+    }
 }
--- 03_SourceCode/Katas/RomanNumberConverter/tests/at/htlleonding/RomanNumberConverter/RomanNumberConverterTest.java
+++ 03_SourceCode/Katas/RomanNumberConverter/tests/at/htlleonding/RomanNumberConverter/RomanNumberConverterTest.java
@@ -1,4 +1,83 @@
+package at.htlleonding.RomanNumberConverter;
+
 import static org.junit.jupiter.api.Assertions.*;
-class RomanNumberConverterTest {
-  
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * unit test for simple App
+ */
+
+public class RomanNumberConverterTest {
+    /*
+     * Rigoures Test
+     * */
+
+    @Test
+    public void itShouldReturnI_Given1(){
+        assertEquals("I", RomanNumberConverter.toRoman(1));
+    }
+
+    @Test
+    public void itShouldReturnII_Given2() {
+        assertEquals("II", RomanNumberConverter.toRoman(2));
+    }
+
+    @Test
+    public void itShouldReturnV_Given5(){
+        assertEquals("V", RomanNumberConverter.toRoman(5));
+    }
+
+    @Test
+    void itShouldReturnVIs_GivenBetween6And8() {
+        assertEquals("VI", RomanNumberConverter.toRoman(6));
+        assertEquals("VII", RomanNumberConverter.toRoman(7));
+    }
+
+    @Test
+    public void itShouldReturnX_Given10(){
+        assertEquals("X", RomanNumberConverter.toRoman(10));
+    }
+
+    @Test
+    public void itShouldReturnXXI_Given21(){
+        assertEquals("XXI", RomanNumberConverter.toRoman(21));
+    }
+
+    @Test
+    public void itShouldReturnIV_Given4(){
+        assertEquals("IV", RomanNumberConverter.toRoman(4));
+    }
+
+    @Test
+    public void itShouldReturnIX_Given9(){
+        assertEquals("IX", RomanNumberConverter.toRoman(9));
+    }
+
+    @Test
+    public void itShouldReturn1_GivenI(){
+        assertEquals(1, RomanNumberConverter.toArabic("I"));
+    }
+
+    @Test
+    public void itShouldReturn2_GivenII(){
+        assertEquals(2, RomanNumberConverter.toArabic("II"));
+    }
+
+    @Test
+    public void itShouldReturn3_GivenIII(){
+        assertEquals(3, RomanNumberConverter.toArabic("III"));
+    }
+
+    @Test
+    public void itShouldReturn4_GivenIV(){
+        assertEquals(4, RomanNumberConverter.toArabic("IV"));
+    }
+
+    @Test
+    public void itShouldReturn5_GivenV(){
+        assertEquals(5, RomanNumberConverter.toArabic("V"));
+    }
+
 }
